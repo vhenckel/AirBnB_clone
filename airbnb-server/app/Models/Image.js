04c0aa8e12ac62604600e65c2ab8a5384047dd65
Vitor Henckel <vitor@henckel.com.br>
2018-07-18 23:15:12 -0300

    Adicionado model de imagens

--- airbnb-server/app/Models/Image.js
+++ airbnb-server/app/Models/Image.js
@@ -1,8 +1,17 @@
 'use strict'
 
+const Env = use('Env')
 const Model = use('Model')
 
 class Image extends Model {
+
+  static get computed () {
+    return ['url']
+  }
+
+  getUrl ({ path }) {
+    return `${ENV.get('APP_URL')}/images/${path}`
+  }
 }
 
 module.exports = Image
